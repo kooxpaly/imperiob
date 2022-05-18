647f3e9d9eb5bb3e9ab198d1e5fbc82390242632
IssacPacheco <isaacpacheco.go@gmail.com>
2022-05-18 10:15:45 -0500

    Se agregan cabeceras

---
+++ includes/cabecera.php
@@ -0,0 +1,10 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // Indica los métodos permitidos.
+    header('Access-Control-Allow-Methods: GET, POST, DELETE');
+    // Indica los encabezados permitidos.
+    header('Access-Control-Allow-Headers: Authorization');
+    http_response_code(204);
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include("includes/cabecera.php"); ?>
 <!DOCTYPE html>
 <html lang="es">
 
