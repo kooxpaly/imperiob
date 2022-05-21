b6f7324d020bafaa5c66ab9540b375c44592ac67
Issac Pacheco <isaacpacheco.go@gmail.com>
2022-05-21 10:41:27 -0500

    Se modifica la cabecera

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -1,6 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN, Access-Control-Allow-Origin');
 header('Access-Control-Expose-Headers', 'Authorization, authenticated');
 header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
 header('Access-Control-Allow-Credentials', 'true');
